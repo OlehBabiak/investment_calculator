721cfac01c9e7e380f11b56a688f45a1c496fb9e
Oleh_Babiak <oleh_babiak@epam.com>
2024-12-17 14:14:43 +0200

    lifted state to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Header from "./components/Header"
+import Results from "./components/Results"
 import UserInputForm from "./components/UserInputForm"
+import { useState } from 'react'
+
+const INITIAL_INPUT_PROPS = [
+  { label: 'INITIAL INVESTMENT', id: 'initialInvestment', value: 10000 },
+  { label: 'ANNUAL INVESTMENT', id: 'annualInvestment', value: 1200 },
+  { label: 'EXPECTED RETURN', id: 'expectedReturn', value: 6 },
+  { label: 'DURATION', id: 'duration', value: 10 },
+]
 
 function App() {
+  const [formState, setFormState] = useState(INITIAL_INPUT_PROPS)
+  function onInputValueChange(id, newValue) {
+    setFormState(prev =>
+      prev.map(el =>
+        el.id === id
+          ? { ...el, value: +newValue } // Оновлюємо об'єкт, якщо id співпадає
+          : el // Повертаємо інші об'єкти без змін
+      )
+    );
+  }
   return (
     <>
       <Header />
-      <UserInputForm />
+      <UserInputForm
+        formState={formState}
+        onInputValueChange={onInputValueChange} />
+      <Results formState={formState} />
     </>
   )
 }
--- src/components/UserInputForm.jsx
+++ src/components/UserInputForm.jsx
@@ -1,26 +1,6 @@
-import { useState } from "react";
 import InputElement from "./InputElement"
 
-const INITIAL_INPUT_PROPS = [
-    { label: 'INITIAL INVESTMENT', id: 'initialInvestment', value: 10000 },
-    { label: 'ANNUAL INVESTMENT', id: 'annualInvestment', value: 1200 },
-    { label: 'EXPECTED RETURN', id: 'expectedReturn', value: 6 },
-    { label: 'DURATION', id: 'duration', value: 10 },
-]
-
-export default function UserInputForm() {
-
-    const [formState, setFormState] = useState(INITIAL_INPUT_PROPS)
-
-    function onInputValueChange(id, newValue) {
-        setFormState(prev =>
-            prev.map(el =>
-                el.id === id
-                    ? { ...el, value: newValue } // Оновлюємо об'єкт, якщо id співпадає
-                    : el // Повертаємо інші об'єкти без змін
-            )
-        );
-    }
+export default function UserInputForm({ formState, onInputValueChange }) {
 
     return (
         <section id="user-input">
